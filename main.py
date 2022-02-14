e369b11508baac07b783c17d74e8f45f7d207067
Emmett <Emmettpeck@gmail.com>
2022-02-14 15:46:56 -0800

    Added rolechecks & error catches

--- main.py
+++ main.py
@@ -11,14 +11,16 @@
 import os
 import discord
 from discord.ext import commands
+from discord.ext.commands import has_permissions, CheckFailure
 from dotenv import load_dotenv
 from dockingPort import portSend
 
 load_dotenv()
 bot = commands.Bot(command_prefix='>') # Set Prefix
 
-role_Whitelist = {'Member', 'Moderator'} #Permissible Roles
+role_Whitelist = {'Member', 'Moderator', 'Liam'} #Permissible Roles
 
+# Hello!
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -29,8 +31,9 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
-
-@bot.command(name='whitelist', help="Usage: >whitelist <arg>.", brief="Whitelists <arg> player on the corresponding channel's server.")
+# >whitelist
+@bot.command(name='whitelist', help=f"Usage: >whitelist <arg>. Requires a {role_Whitelist} role.", brief="Whitelists <arg> player on the corresponding channel's server.")
+@commands.has_any_role(role_Whitelist)
 async def whitelist(ctx, *, mess):
  
     # Check Roles
@@ -40,17 +43,24 @@ async def whitelist(ctx, *, mess):
     # Send command
     response = portSend(ctx.channel.id, f"whitelist add {mess}")
     await ctx.send(response) # Bot response
+@whitelist.error
+async def whitelist_error(error, ctx):
+    if isinstance(error, CheckFailure):
+        await bot.send_message(ctx.message.channel, "You do not have the necessary roles.")
 
-@bot.command(name='send', help="Usage: >send <arg>.", brief="Sends <arg> command to the corresponding channel's server.")
-async def whitelist(ctx, *, mess):
- 
-    # Check Roles
-    if not discord.ext.commands.has_any_role(role_Whitelist):
-        return
-        
+# >send
+@bot.command(name='send', help="Usage: >send <arg>. Requires administrator permissions.", brief="Sends <arg> command to the corresponding channel's server.")
+@has_permissions(administrator=True)
+async def send(ctx, *, mess):
+
     response = portSend(ctx.channel.id, mess)
 
     await ctx.send(response) # Bot response
+@send.error
+async def send_error(error, ctx):
+    if isinstance(error, CheckFailure):
+        await bot.send_message(ctx.message.channel, "You do not have the necessary permissions.")
+
 
 
 bot.run(os.getenv('TOKEN'))
