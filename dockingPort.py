312984a387c18ab40c3a390ee2bbc909f1549820
Emmett <Emmettpeck@gmail.com>
2022-02-14 10:19:48 -0800

    Broke out dockingport function, reorganized.

---
+++ dockingPort.py
@@ -0,0 +1,26 @@
+import subprocess
+
+
+# Temp list to be moved to a serialized file later
+mc_Channels = [ # Channel List of dictionaries
+    {'name':"mc", 'channel_id':942193852058574949, 'docker_name':"build_main_2021_1"},
+    {'name':"liam", 'channel_id':942241180421328896, 'docker_name':"build_liam_2022_1"}
+]
+
+def portSend(channelID, command): # Sends a command to corresponding server ID. Returns a string output of command response.
+    for channel in mc_Channels: # Check all channels in list for channel ID, then execute if found.
+            if channelID == channel.get('channel_id'):
+
+                # Command Execution Via Commandline through Docker
+                dockerName = channel.get('docker_name')
+                resp_bytes = subprocess.Popen(f'docker exec {dockerName} rcon-cli /{command}', stdout=subprocess.PIPE, shell=True, executable="/bin/bash").stdout.read()
+                resp_str = resp_bytes.decode(encoding="utf-8", errors="ignore")
+                
+                # Logging
+                print(f"Sent command /{command} to {dockerName}")
+                print(f'--- {resp_str}')
+                return resp_str
+            else:
+                # CHANNEL NOT FOUND/WRONG CHANNEL MSG
+                return "Channel Not Found. Use command only in 'Minecraft' text channels."
+            
--- main.py
+++ main.py
@@ -6,26 +6,18 @@
 
 #   TO/DO
 #   -   Help Command
-#   -   CHANNEL NOT FOUND/WRONG CHANNEL MSG
 #   -   Check Roles
-#   -   Command send function from serverID & command
 
 import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-import subprocess
+from dockingPort import portSend
 
 load_dotenv()
 bot = commands.Bot(command_prefix='#', help_command=None) # Set Prefix
 
-
 role_Whitelist = {'Member', 'Moderator'} #Permissible Roles
-mc_Channels = [ # Channel List of dictionaries
-    {'name':"mc", 'channel_id':942193852058574949, 'docker_name':"build_main_2021_1"},
-    {'name':"liam", 'channel_id':942241180421328896, 'docker_name':"build_liam_2022_1"}
-]
-
 
 @bot.event
 async def on_message(message):
@@ -45,23 +37,9 @@ async def whitelist(ctx, *, mess):
     if not discord.ext.commands.has_any_role(role_Whitelist):
         return
     
-
-    channelID = ctx.channel.id  # Check Channel to determine server ID to execute through
-
-    for channel in mc_Channels: # Check all channels in list for channel ID, then execute if found.
-            if channelID == channel.get('channel_id'):
-
-                # Command Execution Via Commandline through Docker
-                dockerName = channel.get('docker_name')
-                resp_bytes = subprocess.Popen(f'docker exec {dockerName} rcon-cli /whitelist add {mess}', stdout=subprocess.PIPE, shell=True, executable="/bin/bash").stdout.read()
-                resp_str = resp_bytes.decode(encoding="utf-8", errors="ignore")
-                print(f"Sent command /whitelist add {mess} to {dockerName}")
-                print(f'--- {resp_str}')
-            else:
-                # CHANNEL NOT FOUND/WRONG CHANNEL MSG
-                return
-
-    await ctx.send(resp_str) # Bot response
+    # Send command
+    response = portSend(ctx.channel.id, f"whitelist add {mess}")
+    await ctx.send(response) # Bot response
 
 @bot.command(name='send')
 async def whitelist(ctx, *, mess):
@@ -69,26 +47,10 @@ async def whitelist(ctx, *, mess):
     # Check Roles
     if not discord.ext.commands.has_any_role(role_Whitelist):
         return
+        
+    response = portSend(ctx.channel.id, mess)
 
-
-    channelID = ctx.channel.id  # Check Channel to determine server ID to execute through
-
-    for channel in mc_Channels: # Check all channels in list for channel ID, then execute if found.
-            if channelID == channel.get('channel_id'):
-
-                # Command Execution Via Commandline through Docker
-                dockerName = channel.get('docker_name')
-                resp_bytes = subprocess.Popen(f'docker exec {dockerName} rcon-cli /{mess}', stdout=subprocess.PIPE, shell=True, executable="/bin/bash").stdout.read()
-                resp_str = resp_bytes.decode(encoding="utf-8", errors="ignore")
-               
-                # Console Logging
-                print(f"Sent command /{mess} to {dockerName}")
-                print(f'--- {resp_str}')
-            else:
-                # CHANNEL NOT FOUND/WRONG CHANNEL MSG
-                return
-
-    await ctx.send(resp_str) # Bot response
+    await ctx.send(response) # Bot response
 
 
 bot.run(os.getenv('TOKEN'))
