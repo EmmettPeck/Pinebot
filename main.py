ee72d081ed006df59e22853ddd39f064b41f67e1
emmett <emmettpeck@gmail.com>
2022-02-15 02:54:58 +0000

    Added cogs/owner.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-extensions = ['cogs.utils', 'cogs.social'] #Cogfiles
+extensions = ['cogs.utils', 'cogs.social', 'cogs.owner'] #Cogfiles
 
 def get_prefix(bot, message):
     """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
