039adf2e1deda3eb3610ecffe7521a851bed9425
Emmett Peck <emmettpeck@gmail.com>
2022-06-17 15:03:41 -0700

    1.19 Chat Filter Update #68 - Implemented Challenges - Added warden death

--- app/cogs/minecraft.py
+++ app/cogs/minecraft.py
@@ -185,7 +185,13 @@ class Minecraft(GameCog):
                 msg = f"has made the advancement [{split_first(entry,'[')[1]}"
                 post = get_msg_dict(user, msg, MessageType.ACHIEVEMENT, discord.Color.gold())
 
-            # Death Message Detection -----------------------------------------------------------------------------
+            # Challenge Detection --------------------------------------------------------------------------------
+            elif entry.find("has completed the challenge") >= 0:
+                user = entry.split(' ',1)[0]
+                msg = f"has completed the challenge [{split_first(entry,'[')[1]}"
+                post = get_msg_dict(user, msg, MessageType.ACHIEVEMENT, discord.Color.dark_purple())
+
+            # Death Message Detection ----------------------------------------------------------------------------
             else:
                 dm = Death(entry)
                 if dm.is_death():
@@ -206,7 +212,7 @@ class Death:
         self.msg = msg.strip()
         self.player = self.msg.split()[0]
         self.stripped_msg = self.msg.split(self.player)[1].strip()
-        self.death_msg_startw = ["was shot by","was pummeled by","was pricked to death","walked into a cactus whilst trying to escape","drowned","drowned whilst trying to escape","experienced kinetic energy","experienced kinetic energy whilst trying to escape","blew up","was blown up by","was killed by","hit the ground too hard","fell from a high place","fell off a ladder","fell off some vines","fell off some weeping vines","fell off some twisting vines","fell off scaffolding","fell while climbing","was impaled on a stalagmite","was squashed by a falling anvil","was squashed by a falling block","was skewered by a falling stalactite","went up in flames","burned to death","was burnt to a crisp whilst fighting","went off with a bang","tried to swim in lava","was struck by lightning","discovered the floor was lava","walked into danger zone due to","was killed by magic","was killed by","froze to death","was frozen to death by","was slain by","was fireballed by","was stung to death","was shot by a skull from","starved to death","suffocated in a wall","was squished too much","was squashed by","was poked to death by a sweet berry bush","was killed trying to hurt","was impaled by","fell out of the world","didn't want to live in the same world as","withered away","died from dehydration","died","was roasted in dragon breath","was doomed to fall","fell too far and was finished by","was stung to death by","went off with a bang","was killed by even more magic","was too soft for this world"]
+        self.death_msg_startw = ["was shot by","was pummeled by","was pricked to death","walked into a cactus whilst trying to escape","drowned","drowned whilst trying to escape","experienced kinetic energy","experienced kinetic energy whilst trying to escape","blew up","was blown up by","was killed by","hit the ground too hard","fell from a high place","fell off a ladder","fell off some vines","fell off some weeping vines","fell off some twisting vines","fell off scaffolding","fell while climbing","was impaled on a stalagmite","was squashed by a falling anvil","was squashed by a falling block","was skewered by a falling stalactite","went up in flames","burned to death","was burnt to a crisp whilst fighting","went off with a bang","tried to swim in lava","was struck by lightning","discovered the floor was lava","walked into danger zone due to","was killed by magic","was killed by","froze to death","was frozen to death by","was slain by","was fireballed by","was stung to death","was shot by a skull from","starved to death","suffocated in a wall","was squished too much","was squashed by","was poked to death by a sweet berry bush","was killed trying to hurt","was impaled by","fell out of the world","didn't want to live in the same world as","withered away","died from dehydration","died","was roasted in dragon breath","was doomed to fall","fell too far and was finished by","was stung to death by","went off with a bang","was killed by even more magic","was too soft for this world","was obliterated by a sonically-charged shriek"]
 
     def is_death(self):
         """Checks if playerless string matches death message"""
